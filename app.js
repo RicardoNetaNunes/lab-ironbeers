ef2e8dccb1660b5dee6b5f0e2b111dae17b2383e
Ricardo Nunes <ricardo.agn.nunes@gmail.com>
2021-11-10 19:10:12 +0000

    done

--- app.js
+++ app.js
@@ -14,12 +14,56 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+
+
 // ...
 
 // Add the route handlers here:
 
+
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+
+app.get('/beers', (req, res) => {
+ punkAPI.getBeers()
+  .then((beers) => {
+      
+      res.render('beers.hbs', {beers})
+  })
+  .catch(() => {
+      console.log('Error while fetching beers')
+  })
+
+});
+
+
+
+
+app.get('/random-beer', (req, res) => {
+  
+  punkAPI.getRandom()
+  .then((beers) => {
+      
+    res.render('random-beer', {beers})
+  })
+  .catch(() => {
+      console.log('Error while fetching your randoom beers')
+  })
+
+
+ 
+});
+
+
+
+app.get('*', (req, res) => {
+  res.send('Page not found')
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
